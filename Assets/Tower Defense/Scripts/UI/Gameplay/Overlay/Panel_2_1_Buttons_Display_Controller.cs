63f02b01249d4f8bd90334f5adab9db92ed01ccc
jaisonrobson <xd_jrg@hotmail.com>
2023-06-04 04:38:27 -0300

    chore: created new scripts in preparation for playable structures evolution system & updated todo list with structure evolution system steps

--- Assets/Tower Defense/Scripts/UI/Gameplay/Overlay/Panel_2_1_Buttons_Display_Controller.cs
+++ Assets/Tower Defense/Scripts/UI/Gameplay/Overlay/Panel_2_1_Buttons_Display_Controller.cs
@@ -7,6 +7,7 @@ public class Panel_2_1_Buttons_Display_Controller : MonoBehaviour
 {
     // Public (Variables) [START]
     public GameObject flagButton;
+    public GameObject levelUpButton;
     // Public (Variables) [END]
 
     // (Unity) Methods [START]
@@ -21,16 +22,21 @@ public class Panel_2_1_Buttons_Display_Controller : MonoBehaviour
     private void HandleButtonsDisplay()
     {
         bool canDisplayFlagButtton = false;
+        bool canDisplayLevelUpButton = false;
 
         if (SelectionManager.instance.IsAnythingSelected)
         {
             PlayableStructure ps = SelectionManager.instance.SelectedAgents.FirstOrDefault().GetComponent<PlayableStructure>();
 
             if (ps != null)
+            {
                 canDisplayFlagButtton = true;
+                canDisplayLevelUpButton = true;
+            }
         }
 
         flagButton.SetActive(canDisplayFlagButtton);
+        levelUpButton.SetActive(canDisplayLevelUpButton);
     }
     // Private (Methods) [END]
 }
