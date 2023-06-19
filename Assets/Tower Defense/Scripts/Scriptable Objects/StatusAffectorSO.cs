9fa9aca626d2862c21c93d300cb8dac62d122fe1
jaisonrobson <xd_jrg@hotmail.com>
2023-06-18 21:19:55 -0300

    chore: added SO configuration files for the statuses

--- Assets/Tower Defense/Scripts/Scriptable Objects/StatusAffectorSO.cs
+++ Assets/Tower Defense/Scripts/Scriptable Objects/StatusAffectorSO.cs
@@ -7,14 +7,31 @@ using Sirenix.OdinInspector;
 public class StatusAffectorSO : BaseOptionDataSO
 {
     [BoxGroup("Box1", ShowLabel = false)]
-    [HorizontalGroup("Box1", LabelWidth = 75)]
-
-    [VerticalGroup("Box1")]
     [Required]
     public StatusSO status;
 
-    //FAZER TODAS AS POSSIVEIS VARIAVEIS PARA CONFIGURACOES AQUI DENTRO
-    // COMO POR EX: DANO, DURACAO, TURNOS, ETC.
+    [BoxGroup("Box1")]
+    [Required]
+    [PropertyTooltip("The total duration of the affector.")]
+    [PropertyRange(1, 180)]
+    public int duration = 1;
+
+    [BoxGroup("Box1")]
+    [Required]
+    [PropertyTooltip("The damage percentage each turn will deal to the affected.")]
+    [PropertyRange(0, 100)]
+    public int damage = 0;
+
+    [BoxGroup("Box1")]
+    [Required]
+    [PropertyTooltip("The number of turns will be divided by the total duration equally.")]
+    [PropertyRange(1, 50)]
+    public int turnsQuantity = 1;
+
+    [BoxGroup("Box1")]
+    [PropertyTooltip("Any kind of special condition that needs to be met by the status.")]
+    [PropertyRange(0, 50)]
+    public int specialCondition = 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
